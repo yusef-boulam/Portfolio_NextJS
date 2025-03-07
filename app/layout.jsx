dfae5c37f275db2dc1a1e882ca1af4b9862b0bfb
Yusef Boulam <yusef.boulam@gmail.com>
2025-03-07 21:36:20 +0100

    Ajout du composant Nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Nav from "./components/Nav"; // La barre de navigation de votre site
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Nav />
         {children}
       </body>
     </html>
   );
 }
-
