7d1d559e8ebfaa600d2760165a8a760df9fa1165
Yusef Boulam <yusef.boulam@gmail.com>
2025-03-19 22:28:35 +0100

    Amelioration du CSS sur la Home page

--- app/page.jsx
+++ app/page.jsx
@@ -11,43 +11,55 @@ export default function Home() {
         <meta name="description" content="Portfolio de [Ton Nom]" />
       </Head>
       <main className={styles.main}>
-        <div>
-          <div className={styles.container}>
-            <h1>
-              <span className={styles.hello}>HELLO </span>
-              <span className={styles.nameContainer}>
-                MY NAME IS <span className={styles.name}>YUSEF BOULAM</span>
-              </span>
-            </h1>
-            <h2>
-              <span className={styles.function}>Developpeur Web </span> based in
-              Nice
-            </h2>
+        <div className={styles.container}>
+          <div className={styles.container2}>
+            <div className={styles.containerText}>
+              <h1>
+                <span className={styles.hello}>HELLO </span>
+                <span className={styles.nameContainer}>
+                  MY NAME IS <span className={styles.name}>YUSEF BOULAM</span>
+                </span>
+              </h1>
+              <h2>
+                <span className={styles.function}>Developpeur Web </span> based
+                in Nice
+              </h2>
 
-            <Link href="/contact" className={styles.linkContainer}>
-              <button className={styles.link}>Let's talk with me !</button>
-            </Link>
-          </div>
-          <div className={styles.contact}>
-            <div className={styles.containercallPhone}>
-              <img
-                src="/assets/images/home/phone-call.svg"
-                alt="phone"
-                className={styles.iconeCallPhone}
-              />
-              <p>06 37 05 15 43</p>
+              <Link href="/contact" className={styles.linkContainer}>
+                <button className={styles.link}>Let's talk with me !</button>
+              </Link>
             </div>
-            <div className={styles.containerEmail}>
-              <img
-                src="/assets/images/home/email.svg"
-                alt="phone"
-                className={styles.iconeEmail}
-              />
-              <a href="mailto:yusef.boulam@gmail.com" className={styles.email}>
-                yusef.boulam@gmail.com
-              </a>
+            <div className={styles.contact}>
+              <div className={styles.containercallPhone}>
+                <img
+                  src="/assets/images/home/phone-call.svg"
+                  alt="phone"
+                  className={styles.iconeCallPhone}
+                />
+                <p>06 37 05 15 43</p>
+              </div>
+              <div className={styles.containerEmail}>
+                <img
+                  src="/assets/images/home/email.svg"
+                  alt="phone"
+                  className={styles.iconeEmail}
+                />
+                <a
+                  href="mailto:yusef.boulam@gmail.com"
+                  className={styles.email}
+                >
+                  yusef.boulam@gmail.com
+                </a>
+              </div>
             </div>
           </div>
+        </div>
+        <div className={styles.containerPicture}>
+          <img
+            src="/assets/images/home/email.svg"
+            alt="photo profile"
+            className={styles.pictureProfile}
+          />
         </div>
         <div className={styles.containerSocial}>
           <Link href="https://www.linkedin.com/in/yusef-boulam/">
