5c3bef9ae93c55d5aadac5249d6e412071e235fe
Yusef Boulam <yusef.boulam@gmail.com>
2025-03-24 23:05:13 +0100

    Ajout de l'Age, de la localisation et du diplome + ajout CSS et icones correspondantes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -21,34 +21,63 @@ function about() {
       </section>
       <section className={styles.containerAccroche}>
         <div className={styles.contact}>
-          <div className={styles.containercallPhone}>
-            <img
-              src="/assets/images/home/phone-call.svg"
-              alt="phone"
-              className={styles.iconeCallPhone}
-            />
-            <p>06 37 05 15 43</p>
+          <div>
+            <div className={styles.containercallPhone}>
+              <img
+                src="/assets/images/home/phone-call.svg"
+                alt="phone"
+                className={styles.iconeCallPhone}
+              />
+              <p>06 37 05 15 43</p>
+            </div>
+            <div className={styles.containerEmail}>
+              <img
+                src="/assets/images/home/email.svg"
+                alt="phone"
+                className={styles.iconeEmail}
+              />
+              <a href="mailto:yusef.boulam@gmail.com" className={styles.email}>
+                yusef.boulam@gmail.com
+              </a>
+            </div>
           </div>
-          <div className={styles.containerEmail}>
-            <img
-              src="/assets/images/home/email.svg"
-              alt="phone"
-              className={styles.iconeEmail}
-            />
-            <a href="mailto:yusef.boulam@gmail.com" className={styles.email}>
-              yusef.boulam@gmail.com
-            </a>
+          <div>
+            <div className={styles.containerAge}>
+              <img
+                src="/assets/images/home/utilisateur.svg"
+                alt="age"
+                className={styles.iconeAge}
+              />
+              <p>36 yrs</p>
+            </div>
+
+            <div className={styles.containerTown}>
+              <img
+                src="/assets/images/home/marqueur.svg"
+                alt="Town"
+                className={styles.iconeTown}
+              />
+              <p>Nice</p>
+            </div>
           </div>
         </div>
         <p>
           Développeur web diplômé et passionné, je suis à la recherche d’une
           opportunité pour évoluer professionnellement. Durant ma formation,
-          j’ai développé une application web complète en ReactJS pour la partie
+          j’ai développé une application web complète en ReactJS sur la partie
           front-end et une API RESTful en Node.js pour le back-end.
           Actuellement, je poursuis mon apprentissage en travaillant sur un
           projet Next.js afin d’approfondir mes compétences dans l’écosystème
           React et renforcer mon expertise en développement full-stack.
         </p>
+        <div className={styles.containerDegree}>
+          <img
+            src="/assets/images/home/etudiant.svg"
+            alt="Diplôme"
+            className={styles.iconeDegree}
+          />
+          <p>Développeur Intégrateur Web - Niveau 5 (BAC+2)</p>
+        </div>
       </section>
     </main>
   );
